eff188cdb7c8ba56304fbce40a0c8d3e73d596af
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-09-09 02:41:02 -0300

    feat: ensure AxiosHttpClient calls axios with correct url

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,8 @@
+import { HttpPostClient, HttpPostParams } from '@/data/protocols/http'
+import axios from 'axios'
+
+export class AxiosHttpClient implements HttpPostClient {
+  async post(params: HttpPostParams): Promise<any> {
+    await axios(params.url)
+  }
+}
