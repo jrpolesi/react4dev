ee058dc0c67ea6333700a73f87092306002bfd62
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-10-23 18:24:18 -0300

    refactor: move factories to their own files

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,22 +1,12 @@
-import { RemoteAuthentication } from '@/data/useCases/authentication/remote-authentication'
-import { AxiosHttpClient } from '@/infra/http/axios-http-client/axios-http-client'
+import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
+import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
 import { Login } from '@/presentation/pages'
-import { ValidationComposite } from '@/validation/validators'
-import { ValidationBuilder } from '@/validation/validators/builders/validation-builder'
 import { ReactElement } from 'react'
 
-const apiURL = 'http://fordevs.herokuapp.com/api/login'
+const remoteAuthentication = makeRemoteAuthentication()
+const validationComposite = makeLoginValidation()
 
 export const makeLogin = (): ReactElement => {
-  const axiosHttpClient = new AxiosHttpClient()
-
-  const remoteAuthentication = new RemoteAuthentication(apiURL, axiosHttpClient)
-
-  const validationComposite = ValidationComposite.build([
-    ...ValidationBuilder.field('email').required().email().build(),
-    ...ValidationBuilder.field('password').required().min(5).build()
-  ])
-
   return (
     <Login
       authentication={remoteAuthentication}
