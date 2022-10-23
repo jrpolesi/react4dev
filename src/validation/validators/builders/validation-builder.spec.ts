934f3618d03f163f20d419879ce0cc5bc33c3249
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-10-23 13:20:54 -0300

    test: ensure ValidationBuilder has an email helper

--- src/validation/validators/builders/validation-builder.spec.ts
+++ src/validation/validators/builders/validation-builder.spec.ts
@@ -1,4 +1,5 @@
 import { ValidationBuilder as sut } from '@/validation/validators/builders/validation-builder'
+import { EmailValidation } from '@/validation/validators/email/email-validation'
 import { RequiredFieldValidation } from '@/validation/validators/required-field/required-field-validation'
 
 describe('ValidationBuilder', () => {
@@ -6,5 +7,11 @@ describe('ValidationBuilder', () => {
     const validations = sut.field('any_field').required().build()
 
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+
+  test('Should return EmailValidation', () => {
+    const validations = sut.field('any_field').email().build()
+
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
 })
