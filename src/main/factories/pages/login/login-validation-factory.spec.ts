b4db6aff2be2d545beee9121aa2315d77c7ab245
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-10-23 18:32:10 -0300

    test: ensure ValidationComposite use correct validations

---
+++ src/main/factories/pages/login/login-validation-factory.spec.ts
@@ -0,0 +1,16 @@
+import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
+import { ValidationComposite } from '@/validation/validators'
+import { ValidationBuilder } from '@/validation/validators/builders/validation-builder'
+
+describe('LoginValidationFactory', () => {
+  test('Should compose ValidationComposite with correct validations', () => {
+    const composite = makeLoginValidation()
+
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...ValidationBuilder.field('email').required().email().build(),
+        ...ValidationBuilder.field('password').required().min(5).build()
+      ])
+    )
+  })
+})
