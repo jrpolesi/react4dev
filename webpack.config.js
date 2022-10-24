806ef62c54e22f5eb4201c2d0170dda55350cc13
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-10-23 21:18:41 -0300

    feat: add env var to API URL

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const { DefinePlugin } = require('webpack')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
@@ -48,5 +49,10 @@ module.exports = {
     react: 'React',
     'react-dom': 'ReactDOM'
   },
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new CleanWebpackPlugin(),
+    new DefinePlugin({
+      'process.env.API_URL': JSON.stringify('http://fordevs.herokuapp.com/api')
+    })
+  ]
 }
