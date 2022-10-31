4d1aadf167d7d3e7a4ac9aab7f988342770b4fe8
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-10-31 02:00:59 -0300

    feat: change Login composition to add SaveAccessToken

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,16 +1,19 @@
 import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
 import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
+import { makeLocalSaveAccessToken } from '@/main/factories/usecases/save-access-token/local-save-access-token-factory'
 import { Login } from '@/presentation/pages'
 import { ReactElement } from 'react'
 
 const remoteAuthentication = makeRemoteAuthentication()
 const validationComposite = makeLoginValidation()
+const saveAccessToken = makeLocalSaveAccessToken()
 
 export const makeLogin = (): ReactElement => {
   return (
     <Login
       authentication={remoteAuthentication}
       validation={validationComposite}
+      saveAccessToken={saveAccessToken}
     />
   )
 }
