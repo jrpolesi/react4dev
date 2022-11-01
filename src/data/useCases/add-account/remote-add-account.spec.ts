058e7fc850a9ca69480cd740c2bc161788d20654
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-10-31 21:56:01 -0300

    test: ensure RemoteAddAccount throws EmailInUseError if HttpPostClient returns 403

--- src/data/useCases/add-account/remote-add-account.spec.ts
+++ src/data/useCases/add-account/remote-add-account.spec.ts
@@ -1,4 +1,6 @@
+import { HttpStatusCode } from '@/data/protocols/http'
 import { HttpPostClientSpy } from '@/data/test'
+import { EmailInUseError } from '@/domain/errors'
 import { AccountModel } from '@/domain/models'
 import { mockAddAccountParams } from '@/domain/test'
 import { AddAccountParams } from '@/domain/useCases'
@@ -33,12 +35,26 @@ describe('AddAccount', () => {
   })
 
   test('Should call HttpPostClient with correct body', async () => {
-    const authParams = mockAddAccountParams()
+    const addAccountParams = mockAddAccountParams()
 
     const { sut, httpPostClientSpy } = makeSut()
 
-    await sut.add(authParams)
+    await sut.add(addAccountParams)
 
-    expect(httpPostClientSpy.body).toEqual(authParams)
+    expect(httpPostClientSpy.body).toEqual(addAccountParams)
+  })
+
+  test('Should throw EmailInUseError if HttpPostClient returns 403', async () => {
+    const addAccountParams = mockAddAccountParams()
+
+    const { sut, httpPostClientSpy } = makeSut()
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+
+    const promise = sut.add(addAccountParams)
+
+    await expect(promise).rejects.toThrow(new EmailInUseError())
   })
 })
