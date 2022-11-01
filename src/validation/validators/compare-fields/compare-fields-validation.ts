c5cddd4fe18bcf64e72ce037218ea170ae7d4a45
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-10-31 22:25:23 -0300

    feat: ensure CompareFieldsValidation returns error if validation fails

---
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -0,0 +1,13 @@
+import { InvalidFieldError } from '@/validation/errors'
+import { FieldValidation } from '@/validation/protocols/field-validation'
+
+export class CompareFieldsValidation implements FieldValidation {
+  constructor(readonly field: string, private readonly valueToCompare: string) {
+    this.field = field
+    this.valueToCompare = valueToCompare
+  }
+
+  validate(value: string): Error | null {
+    return new InvalidFieldError()
+  }
+}
