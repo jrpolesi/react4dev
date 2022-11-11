8ccf887744976a1b83ff48c4be5df792e834079a
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-11-10 23:37:35 -0300

    test: ensure Login present error message if invalid credentials are provided

--- src/main/test/cypress/integration/login.spec.cy.ts
+++ src/main/test/cypress/integration/login.spec.cy.ts
@@ -1,5 +1,7 @@
 import { faker } from '@faker-js/faker'
 
+const baseURL = Cypress.config().baseUrl
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -51,5 +53,29 @@ describe('Login', () => {
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
 
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+
+    cy.getByTestId('submit').click()
+
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+      .getByTestId('spinner')
+      .should('not.exist')
+      .getByTestId('main-error')
+      .should('contain.text', 'Credenciais inválidas')
+
+    if (baseURL) {
+      cy.url().should('eq', `${baseURL}/login`)
+    } else {
+      throw new Error('Cypress baseUrl not found')
+    }
   })
 })
