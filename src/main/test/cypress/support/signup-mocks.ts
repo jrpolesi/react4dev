d23d2ddfe99cd3baedf6ddfea75c0ae9f3e53b31
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-11-12 13:24:06 -0300

    test: ensure Signup present correct error if server fails

--- src/main/test/cypress/support/signup-mocks.ts
+++ src/main/test/cypress/support/signup-mocks.ts
@@ -4,3 +4,6 @@ const URL_LOGIN_REGEXP = /api\/signup/
 
 export const mockEmailInUseError = (): void =>
   Helper.mockEmailInUseError(URL_LOGIN_REGEXP)
+
+export const mockUnexpectedError = (): void =>
+  Helper.mockUnexpectedError(URL_LOGIN_REGEXP, 'POST')
