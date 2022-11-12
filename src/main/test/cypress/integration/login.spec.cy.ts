53eb127ea4b0d06d29a2563ce159e2a2c8b762f3
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-11-12 01:07:02 -0300

    test: ensure Login will not submit form if is invalid

--- src/main/test/cypress/integration/login.spec.cy.ts
+++ src/main/test/cypress/integration/login.spec.cy.ts
@@ -209,4 +209,18 @@ describe('Login', () => {
 
     cy.get('@request.all').should('have.length', 1)
   })
+
+  it('Should not call submit if form is invalid', () => {
+    cy.intercept('POST', /api\/login/, {
+      statusCode: 200,
+      body: {
+        accessToken: faker.random.words()
+      },
+      delay: 100
+    }).as('request')
+
+    cy.getByTestId('email').focus().type(faker.internet.email()).type('{enter')
+
+    cy.get('@request.all').should('have.length', 0)
+  })
 })
