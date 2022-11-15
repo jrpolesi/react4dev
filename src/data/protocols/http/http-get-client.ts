0ef2a41f36374542312d79cef2a33ac3f171cbbb
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-11-14 23:14:23 -0300

    feat: ensure RemoteLoadSurveyList throws UnexpectedError on 403

--- src/data/protocols/http/http-get-client.ts
+++ src/data/protocols/http/http-get-client.ts
@@ -1,7 +1,9 @@
+import { HttpResponse } from '@/data/protocols/http/http-response'
+
 export type HttpGetParams = {
   url: string
 }
 
-export interface HttpGetClient {
-  get: (params: HttpGetParams) => Promise<void>
+export interface HttpGetClient<TBodyResponse extends object = {}> {
+  get: (params: HttpGetParams) => Promise<HttpResponse<TBodyResponse>>
 }
---
+++ src/data/useCases/load-survey-list/remote-load-survey-list.ts
@@ -0,0 +1,25 @@
+import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
+import { UnexpectedError } from '@/domain/errors'
+import { SurveyModel } from '@/domain/models'
+import { LoadSurveyList } from '@/domain/useCases/load-survey-list'
+
+export class RemoteLoadSurveyList implements LoadSurveyList {
+  constructor(
+    private readonly url: string,
+    private readonly httpGetClient: HttpGetClient<SurveyModel[]>
+  ) {
+    this.url = url
+    this.httpGetClient = httpGetClient
+  }
+
+  async loadAll(): Promise<SurveyModel[]> {
+    const httpResponse = await this.httpGetClient.get({ url: this.url })
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return httpResponse.body ?? []
+      default:
+        throw new UnexpectedError()
+    }
+  }
+}
