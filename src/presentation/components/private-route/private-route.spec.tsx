790d1d048f8f0cdb225995a2754f8fb4b1eef929
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-12-27 00:42:52 -0300

    test: ensure PrivateRoute renders component if token is not empty

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,21 +1,39 @@
+import { AccountModel } from '@/domain/models'
+import { mockAccountModel } from '@/domain/test'
 import PrivateRoute from '@/presentation/components/private-route/private-route'
+import { ApiContext } from '@/presentation/contexts'
 import { render } from '@testing-library/react'
+import 'jest-localstorage-mock'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-const makeSut = (): void => {
+const makeSut = (account = mockAccountModel()): void => {
   render(
     <BrowserRouter>
-      <Routes>
-        <Route index element={<PrivateRoute />} />
-      </Routes>
+      <ApiContext.Provider
+        value={{
+          getCurrentAccount: () => account
+        }}
+      >
+        <Routes>
+          <Route index element={<PrivateRoute component={<></>} />} />
+        </Routes>
+      </ApiContext.Provider>
     </BrowserRouter>
   )
 }
 
 describe('PrivateRoute', () => {
+  afterEach(() => history.replaceState({}, '', '/'))
+
   test('Should redirect to /login if token is empty', () => {
-    makeSut()
+    makeSut(null as unknown as AccountModel)
 
     expect(location.pathname).toBe('/login')
+  })
+
+  test('Should render current component if token is not empty', () => {
+    makeSut()
+
+    expect(location.pathname).toBe('/')
   })
 })
