63ba7494ff199862a4eb4789aa88bdf1cd1470fc
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-12-28 19:15:05 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/header/header.spec.tsx
+++ src/presentation/components/header/header.spec.tsx
@@ -1,19 +1,32 @@
+import { AccountModel } from '@/domain/models'
 import Header from '@/presentation/components/header/header'
 import ApiContext from '@/presentation/contexts/api/api-context'
 import { fireEvent, render, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 
+type SutTypes = {
+  setCurrentAccountMock: (account: AccountModel) => void
+}
+
+const makeSut = (): SutTypes => {
+  const setCurrentAccountMock = jest.fn()
+
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </ApiContext.Provider>
+  )
+
+  return {
+    setCurrentAccountMock
+  }
+}
+
 describe('Header Componente', () => {
   test('Should call setCurrentAccount with null', () => {
-    const setCurrentAccountMock = jest.fn()
-
-    render(
-      <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
-        <BrowserRouter>
-          <Header />
-        </BrowserRouter>
-      </ApiContext.Provider>
-    )
+    const { setCurrentAccountMock } = makeSut()
 
     fireEvent.click(screen.getByTestId('logout'))
 
