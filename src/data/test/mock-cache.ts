9b825a572c5d7652251a0ee5f09942ca03e6987c
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-12-28 11:38:07 -0300

    test: ensure AuthorizeHttpGetClientDecorator add token to HttpGetClient

--- src/data/test/mock-cache.ts
+++ src/data/test/mock-cache.ts
@@ -3,7 +3,7 @@ import { faker } from '@faker-js/faker'
 
 export class GetStorageSpy implements GetStorage {
   key: string = ''
-  value = { [faker.random.word()]: faker.random.words() }
+  value: any = { [faker.random.word()]: faker.random.words() }
 
   get(key: string): any {
     this.key = key
