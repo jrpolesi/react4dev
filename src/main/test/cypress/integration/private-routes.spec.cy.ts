4ec339bf1668e3ca8cabe92b278b62f3d32af396
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-12-29 11:09:02 -0300

    test: ensure SurveyList logout if no token is found

---
+++ src/main/test/cypress/integration/private-routes.spec.cy.ts
@@ -0,0 +1,8 @@
+import * as Helper from '@/main/test/cypress/support/helpers'
+
+describe('PrivateRoutes', () => {
+  it('Should logout if survey-list has no token', () => {
+    cy.visit('')
+    Helper.testUrl('/login')
+  })
+})
