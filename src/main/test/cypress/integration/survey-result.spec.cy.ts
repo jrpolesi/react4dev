3e9ec2df6d0c48ccbbfd20bd41754f4c23101d8e
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2023-01-15 01:27:46 -0300

    test: ensure SurveyResult logout on AccessDenied error

--- src/main/test/cypress/integration/survey-result.spec.cy.ts
+++ src/main/test/cypress/integration/survey-result.spec.cy.ts
@@ -99,6 +99,9 @@ describe('SurveyResult', () => {
     const mockUnexpectedError = (): void =>
       Http.mockServerError(URL_SURVEYS_REGEXP, 'PUT')
 
+    const mockAccessDeniedError = (): void =>
+      Http.mockForbiddenError(URL_SURVEYS_REGEXP, 'PUT')
+
     beforeEach(() => {
       cy.fixture('account').then((account) => {
         Helper.setLocalStorageItem('account', account)
@@ -118,6 +121,14 @@ describe('SurveyResult', () => {
         'contain.text',
         'Algo de errado aconteceu. Tente novamente em breve.'
       )
+    })
+
+    it('Should logout on AccessDeniedError', () => {
+      mockAccessDeniedError()
+
+      cy.get('li:nth-child(2)').click()
+
+      Helper.testUrl('/login')
     })
   })
 })
